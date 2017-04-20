cb4443331052937c19ed2e5b80ca22f4ed6d20c7
97Sandy <97sandyrdz@gmail.com>
2017-04-20 07:57:17 -0500

    Initial commit

---
+++ app/src/main/java/mx/itesm/metodosnumericos/MetodosNumericos.java
@@ -0,0 +1,13 @@
+package mx.itesm.metodosnumericos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MetodosNumericos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metodos_numericos);
+    }
+}
